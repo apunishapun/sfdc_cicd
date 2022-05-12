361d84023184e92f34fbc4de7a5fc76a345f1f06
Booz Espiridion <apunishapun@gmail.com>
2022-05-12 09:58:35 +0800

    Dev

--- scripts/js/sfbuilds/create-auth-url.js
+++ scripts/js/sfbuilds/create-auth-url.js
@@ -1,12 +1,5 @@
 const fs = require('fs');
 
-const args = process.argv.slice(2);
-if (args.length > 0) {
-    createAuthUrl(args[0]);
-} else {
-    console.error('Required arg missing');
-}
-
 const createAuthUrl = (sfdxUrl) => {
     try {
         fs.writeFileSync('bin/sfdx-url.json', `"{ "sfdxAuthUrl": "${sfdxUrl}" }"`, 'utf-8');
@@ -15,3 +8,14 @@ const createAuthUrl = (sfdxUrl) => {
         console.error(err);
     }
 }
+
+function init() {
+    const args = process.argv.slice(2);
+    if (args.length > 0) {
+        createAuthUrl(args[0]);
+    } else {
+        console.error('Required arg missing');
+    }
+}
+
+init();
